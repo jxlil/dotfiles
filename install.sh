9cb616e529150a0c54b08b2cf617f21ba15f68fb
Jalil SA <61639983+jxlil@users.noreply.github.com>
2022-03-21 22:41:04 -0600

    fix: install_neovim

--- install.sh
+++ install.sh
@@ -43,7 +43,7 @@ function install_nvm() {
 
 function install_neovim() {
 	echo "${bold}=> Installing neovim${norm}"
-
+	mkdir -p ~/.local/bin
 	curl -L https://github.com/neovim/neovim/releases/latest/download/nvim.appimage -o $HOME/.local/bin/nvim
 	chmod u+x $HOME/.local/bin/nvim
 
