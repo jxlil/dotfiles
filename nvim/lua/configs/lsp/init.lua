5888aa30c5a4ea0d0eca801dd4e4f8ac299cbf51
Jalil SA <61639983+jxlil@users.noreply.github.com>
2022-11-10 09:34:16 -0600

    fix: neovim formatting

--- nvim/lua/configs/lsp/init.lua
+++ nvim/lua/configs/lsp/init.lua
@@ -11,7 +11,7 @@ local null_ls_opts = lsp.build_options("null-ls", {
             vim.api.nvim_create_autocmd("BufWritePre", {
                 desc = "Auto format before save",
                 pattern = "<buffer>",
-                callback = vim.lsp.buf.format,
+                callback = vim.lsp.buf.formatting,
             })
         end
     end
@@ -22,7 +22,7 @@ local formatting = null_ls.builtins.formatting
 null_ls.setup({
     on_attach = null_ls_opts.on_attach,
     sources = {
-        formatting.black.with({extra_args = {"--fast"}}),
+        formatting.black.with({extra_args = {"--fast", "--line-length=200"}}),
         formatting.prettier
     }
 })
@@ -33,6 +33,5 @@ lsp.setup()
 lspconfig.tsserver.setup {
     on_attach = function(client)
         client.server_capabilities.documentFormattingProvider = false
-
     end
 } 
