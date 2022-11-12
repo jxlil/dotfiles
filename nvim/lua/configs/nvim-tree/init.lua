82bd896df9366601fa918afdecb95aeb5161f0f2
Jalil SA <61639983+jxlil@users.noreply.github.com>
2022-11-11 21:25:58 -0600

    change (neovim): git icons in nvim-tree

--- nvim/lua/configs/nvim-tree/init.lua
+++ nvim/lua/configs/nvim-tree/init.lua
@@ -1,5 +1,21 @@
-require("nvim-tree").setup{
+
+require("nvim-tree").setup {
     view = {
         width = 35
-    }
+    },
+    renderer = {
+        icons = {
+            glyphs = {
+                git = {
+                    unstaged = "!",
+                    staged = "+",
+                    unmerged = "#",
+                    renamed = ">",
+                    untracked = "?",
+                    deleted = "-",
+                    ignored = "*",
+                },
+            },
+        },
+    },
 }
