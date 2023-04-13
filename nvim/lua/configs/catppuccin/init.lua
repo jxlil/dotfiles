d7abb0d293a15322f273234b9e55aa6882a5ecdb
Jalil SA <61639983+jxlil@users.noreply.github.com>
2023-04-13 17:29:13 -0500

    remove old configs

--- nvim/lua/configs/catppuccin/init.lua
+++
@@ -1,8 +0,0 @@
-vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
-
-require("catppuccin").setup({
-	transparent_background = true,
-    term_colors = true
-})
-
-vim.cmd [[colorscheme catppuccin]]
