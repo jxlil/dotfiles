e38637f49412aa9dc55269a7d40e96305d8c3006
Jalil SA <61639983+jxlil@users.noreply.github.com>
2023-04-19 17:31:26 -0500

    *

--- nvim/lua/configs/lsp/init.lua
+++ nvim/lua/configs/lsp/init.lua
@@ -16,9 +16,9 @@ local null_ls = require("null-ls")
 null_ls.setup({
 	sources = {
 		null_ls.builtins.diagnostics.flake8,
-		null_ls.builtins.formatting.isort.with({
-			extra_args = { "--profile=black" },
-		}),
+        null_ls.builtins.formatting.isort.with({
+            extra_args = { "--profile=black" },
+        }),
 		null_ls.builtins.formatting.black.with({
 			extra_args = { "--fast", "--line-length=200" },
 		}),
