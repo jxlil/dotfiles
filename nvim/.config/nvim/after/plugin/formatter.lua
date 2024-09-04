10d42877b1629e872796a81b7fd3751ddf02f92f
Jalil SA <61639983+jxlil@users.noreply.github.com>
2024-09-04 10:00:56 -0600

    added go to nvim/formatter.lua

--- nvim/.config/nvim/after/plugin/formatter.lua
+++ nvim/.config/nvim/after/plugin/formatter.lua
@@ -6,6 +6,22 @@ require("formatter").setup {
   logging = false,
   log_level = vim.log.levels.WARN,
   filetype = {
+      go = {
+        function()
+          return {
+            exe = "gofmt",
+            args = {"-s"},
+            stdin = true
+          }
+        end,
+        function()
+          return {
+            exe = "goimports",
+            args = {"-local", "github.com/"},
+            stdin = true
+          }
+        end,
+      },
       python = {
           function()
               return {
@@ -52,9 +68,9 @@ require("formatter").setup {
 vim.keymap.set("n", "<leader>f", vim.cmd.Format)
 vim.keymap.set("n", "<leader>F", vim.cmd.FormatWrite)
 
--- vim.cmd([[
---     augroup FormatAutogroup
---         autocmd!
---         autocmd BufWritePost * FormatWrite
---     augroup END
--- ]])
+vim.cmd([[
+    augroup FormatAutogroup
+        autocmd!
+        autocmd BufWritePost * FormatWrite
+    augroup END
+]])
